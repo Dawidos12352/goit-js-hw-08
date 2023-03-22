977567bd9e199742a8536f6ae7faea5df956d1f2
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2023-03-22 18:03:24 +0100

    first task ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryArray = [];
+
+galleryItems.forEach(event => {
+const galleryItem = document.createElement("a");
+galleryItem.className = "gallery__item";
+galleryItem.href = event.original;
+
+const galleryImage = document.createElement("img");
+galleryImage.className = "gallery__image";
+galleryImage.src = event.preview;
+galleryImage.alt = event.description;
+
+galleryItem.append(galleryImage);
+galleryArray.push(galleryItem);
+});
+
+gallery.append(...galleryArray);
+
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
